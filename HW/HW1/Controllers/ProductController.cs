8e87cf34605fc05a37252cd8fcaec2f9356e3eec
MG <maryamvh@code.edu.az>
2023-04-18 22:50:12 +0400

    done

---
+++ HW/HW1/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HW1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ContentResult Details( int id, string name, decimal price)
+       {
+           ContentResult result = new ContentResult();
+           result.Content = "Product detail - Id: " + id + "  name: " + name + "  price:" + price;
+           return result;
+       }
+    }
+}
